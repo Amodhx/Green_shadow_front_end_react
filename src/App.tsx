ca2b1b616cd4342dff3504fe16aaf84ba6e1d426
Amodhx <amxdhnanditha@gmail.com>
2024-12-23 10:02:33 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SignIn from "./pages/SignIn.tsx";
+
+function App() {
+
+  return (
+    <>
+        <SignIn/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,8 @@
+function SignIn(){
+    return(
+        <>
+            <h5 className='text-red-600'>Sign In Page Loaded</h5>
+        </>
+    )
+}
+export default SignIn;
