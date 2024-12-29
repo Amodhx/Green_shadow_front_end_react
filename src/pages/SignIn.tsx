4d08732a6d1ecec13d13da4718df7a33d391f4b6
Amodhx <amxdhnanditha@gmail.com>
2024-12-29 10:14:35 +0530

    added logo and main company name

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,11 +1,19 @@
 import "../css/SignIn.css"
+import greenShadowLogo from '../assets/Green-shadow-logo.png';
 
 function SignIn(){
     return(
         <>
             <section id='loginForm'>
                 <div id='signInSection'  className='flex flex-row absolute inset-0'>
-                    <div className='bg-amber-200'></div>
+                    <div>
+                        <div className='mt-12 text-center place-items-center'>
+                            <img src={greenShadowLogo} alt="Logo" width='85px'/>
+                            <h3 className='font-medium'>Green Shadow</h3>
+                            <p>Please Log in to your Account!</p>
+                        </div>
+
+                    </div>
                     <div className='bg-amber-100'></div>
                 </div>
             </section>
