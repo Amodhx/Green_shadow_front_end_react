6b99b8bcc916280a5b24be0788da666e8b6435c8
Amodhx <amxdhnanditha@gmail.com>
2024-12-29 10:35:57 +0530

    created sign in form

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -12,12 +12,32 @@ function SignIn(){
                             <h3 className='font-medium'>Green Shadow</h3>
                             <p>Please Log in to your Account!</p>
                         </div>
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <form className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <input type="email" name="email" id="email"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           placeholder="name@company.com"/>
+                                </div>
+                                <div>
+                                    <input type="password" name="password" id="password" placeholder="••••••••"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                                </div>
+                                <button id='submitBtn' type="submit"
+                                        className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in
+                                </button>
+                            </form>
+                        </div>
+
 
                     </div>
-                    <div className='bg-amber-100'></div>
+                    <div>
+
+                    </div>
                 </div>
             </section>
         </>
     )
 }
+
 export default SignIn;
