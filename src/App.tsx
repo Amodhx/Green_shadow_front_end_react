1a4868a60075cccfda4c1f2036999629334a3035
Amodhx <amxdhnanditha@gmail.com>
2025-01-09 11:34:01 +0530

    added RootLayout child paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import {createBrowserRouter, RouterProvider} from "react-router";
 import RootLayout from "./components/RootLayout.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
 import Staff from "./pages/Staff.tsx";
+import Crops from "./pages/Crops.tsx";
+import Equipment from "./pages/Equipment.tsx";
+import Fields from "./pages/Fields.tsx";
+import Vehicle from "./pages/Vehicle.tsx";
+import Logs from "./pages/Logs.tsx";
 
 function App() {
 
@@ -19,7 +24,11 @@ function App() {
                 {path : '' , element : <Dashboard/>},
                 {path : 'dashboard' , element : <Dashboard/>},
                 {path : 'staff', element : <Staff/>},
-                // {path : '/items', element : <Items/>}
+                {path : 'crops', element : <Crops/>},
+                {path: 'equipments', element : <Equipment/>},
+                {path: 'fields', element : <Fields/>},
+                {path: 'vehicles', element : <Vehicle/>},
+                {path: 'logs', element : <Logs/>},
             ]
         }
     ]);
