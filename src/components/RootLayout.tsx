2d8dbb9005251f942f28785706abeb6294221df2
Amodhx <amxdhnanditha@gmail.com>
2025-01-10 10:52:39 +0530

    recreated side bar cause have a bug

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,12 +1,22 @@
-import Sidebar from "./Sidebar.tsx";
 import {Outlet} from "react-router";
+import SideBar from "./SideBar.tsx";
 
 function RootLayout(){
     return(
         <>
-            <Sidebar></Sidebar>
-            <Outlet></Outlet>
+            <div className="h-screen w-screen flex">
+                <div className="w-[100px]">
+                    <SideBar/>
+                </div>
+
+                {/* Main Content */}
+                <div className="flex-1 bg-white p-6 overflow-x-hidden overflow-y-auto ">
+                    <Outlet/>
+                </div>
+            </div>
+
         </>
     )
 }
+
 export default RootLayout;
