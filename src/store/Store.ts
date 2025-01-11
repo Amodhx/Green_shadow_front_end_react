4d297b888cf4da8d9b88c4790d62ded59c27e938
Amodhx <amxdhnanditha@gmail.com>
2025-01-11 20:04:37 +0530

    updated Store.ts to equipments

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import VehicleSlice from "../slices/VehicleSlice.ts";
+import EquipmentSlice from "../slices/EquipmentSlice.ts";
 
 export const store = configureStore({
     reducer:{
         vehicles : VehicleSlice,
+        equipments : EquipmentSlice
     }
 })
