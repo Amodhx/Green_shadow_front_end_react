63ce675e30da60b41c88850c520049735f841853
Amodhx <amxdhnanditha@gmail.com>
2025-01-12 22:11:11 +0530

    created staff Modal

--- src/components/Staff Page/StaffModal.tsx
+++ src/components/Staff Page/StaffModal.tsx
@@ -9,6 +9,9 @@ function StaffModal({closeModal,selectedStaff}: {closeModal: () => void,selected
         }
     };
     useEffect(() => {
+        setFieldIds([...fieldIds,'F001','F002'])
+        setEquipmentIds([...equipmentIds,'E001','E002'])
+        setVehicleIds([...vehicleIds,'V001','V002'])
         if (selectedStaff){
             setButtonText("Update Staff")
         }
@@ -26,7 +29,53 @@ function StaffModal({closeModal,selectedStaff}: {closeModal: () => void,selected
     const [buttonText,setButtonText] = useState('Save Staff')
 
 
+    const [additionalFields,setAdditionalFields] = useState<string[]>([])
+    const [additionalVehicles,setAdditionalVehicles] = useState<string[]>([])
+    const [additionalEquipments,setAdditionalEquipments] = useState<string[]>([])
 
+    const [fieldIds,setFieldIds] = useState<string[]>(['Select Field Id'])
+    const [vehicleIds,setVehicleIds] = useState<string[]>(['Select Vehicle Id'])
+    const [equipmentIds,setEquipmentIds] = useState<string[]>(['Select Equipment Id'])
+
+    const addFieldDropdown = ()=>{
+        setAdditionalFields([...additionalFields,'']);
+    }
+    const addVehicleDropdown = ()=>{
+        setAdditionalVehicles([...additionalVehicles,'']);
+    }
+    const addEquipmentDropdown = ()=>{
+        setAdditionalEquipments([...additionalEquipments,'']);
+    }
+    function removeFieldIdSelector(index:number){
+        const updatedFields = [...additionalFields]
+        updatedFields.splice(index,1)
+        setAdditionalFields(updatedFields);
+    }
+    function removeVehicleIdSelector(index:number){
+        const updatedVehicle = [...additionalVehicles]
+        updatedVehicle.splice(index,1)
+        setAdditionalVehicles(updatedVehicle);
+    }
+    function removeEquipmentIdSelector(index:number){
+        const updatedEquipment = [...additionalEquipments]
+        updatedEquipment.splice(index,1)
+        setAdditionalEquipments(updatedEquipment);
+    }
+    function setSelectedFieldId(index:number , e:any){
+        const updatedField = [...additionalFields];
+        updatedField[index] = e.target.value;
+        setAdditionalFields(updatedField)
+    }
+    function setSelectedVehicleId(index:number , e:any){
+        const updatedVehicle = [...additionalVehicles];
+        updatedVehicle[index] = e.target.value;
+        setAdditionalVehicles(updatedVehicle)
+    }
+    function setSelectedEquipmentId(index:number , e:any){
+        const updatedEquipment = [...additionalEquipments];
+        updatedEquipment[index] = e.target.value;
+        setAdditionalEquipments(updatedEquipment)
+    }
     return(
         <>
             <div
@@ -170,7 +219,171 @@ function StaffModal({closeModal,selectedStaff}: {closeModal: () => void,selected
                                     </select>
                                 </div>
                             </div>
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+                                {/* Field List */}
+                                <div>
+                                    <div className="flex justify-between items-center mb-2">
+                                        <label className="form-label">
+                                            Field
+                                        </label>
+                                        <button
+                                            onClick={addFieldDropdown}
+                                            type="button"
+                                            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
+                                        >
+                                            Add Field
+                                        </button>
+                                    </div>
+                                    <select
+                                        name="staffEquipment"
+                                        className="mt-2 mb-2 form-control p-2 rounded-md w-full text-gray-500"
+                                    >
+                                        <option>Select Field Id</option>
+                                    </select>
+                                    {additionalFields.map((_, index) => (
+                                        <>
+                                            <div key={index} className={'flex'}>
+                                                <div className={'w-[200px]'}>
+                                                    <select
+                                                        value={selectedStaff?.field_list[index]}
+                                                        onChange={(e) => {
+                                                            setSelectedFieldId(index, e)
+                                                        }}
+                                                        key={index}
+                                                        className="mt-2 mb-2 form-control p-2 rounded-md w-full text-gray-500"
+                                                    >
+                                                        {fieldIds.map((e) => (
+                                                            <option key={e} value={e}>{e}</option>
+                                                        ))}
+                                                    </select>
+                                                </div>
+                                                <div>
+                                                    <button
+                                                        key={index}
+                                                        onClick={() => {
+                                                            removeFieldIdSelector(index)
+                                                        }}
+                                                        className={'mt-2 ml-4 h-[40px] w-[70px] bg-green-500 text-white rounded-md hover:bg-red-500'}>Remove
+                                                    </button>
+                                                </div>
 
+
+                                            </div>
+                                        </>
+                                    ))}
+                                </div>
+                                {/*Equipment List*/}
+                                <div>
+                                    <div className="flex justify-between items-center mb-2">
+                                        <label className="form-label">
+                                            Equipment
+                                        </label>
+                                        <button
+                                            onClick={addEquipmentDropdown}
+                                            type="button"
+                                            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
+                                        >
+                                            Add Equipment
+                                        </button>
+                                    </div>
+                                    <select
+                                        name="fieldEquipment"
+                                        className="mt-2 mb-2 form-control p-2 rounded-md w-full text-gray-500"
+                                    >
+                                        <option>Select Field Id</option>
+                                    </select>
+                                    {additionalEquipments.map((_, index) => (
+                                        <>
+                                            <div key={index} className={'flex'}>
+                                                <div className={'w-[200px]'}>
+                                                    <select
+                                                        value={selectedStaff?.equipment_list[index]}
+                                                        onChange={(e) => {
+                                                            setSelectedEquipmentId(index, e)
+                                                        }}
+                                                        key={index}
+                                                        className="mt-2 mb-2 form-control p-2 rounded-md w-full text-gray-500"
+                                                    >
+                                                        {equipmentIds.map((e) => (
+                                                            <option key={e} value={e}>{e}</option>
+                                                        ))}
+                                                    </select>
+                                                </div>
+                                                <div>
+                                                    <button
+                                                        key={index}
+                                                        onClick={() => {
+                                                            removeEquipmentIdSelector(index)
+                                                        }}
+                                                        className={'mt-2 ml-4 h-[40px] w-[70px] bg-green-500 text-white rounded-md hover:bg-red-500'}>Remove
+                                                    </button>
+                                                </div>
+
+
+                                            </div>
+                                        </>
+                                    ))}
+                                </div>
+
+                            </div>
+
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+                                {/* Vehicle List */}
+                                <div>
+                                    <div className="flex justify-between items-center mb-2">
+                                        <label className="form-label">
+                                            Vehicle
+                                        </label>
+                                        <button
+                                            onClick={addVehicleDropdown}
+                                            type="button"
+                                            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
+                                        >
+                                            Add Vehicle
+                                        </button>
+                                    </div>
+                                    <select
+                                        className="mt-2 mb-2 form-control p-2 rounded-md w-full text-gray-500"
+                                    >
+                                        <option>Select Vehicle Id</option>
+                                    </select>
+                                    {additionalVehicles.map((_, index) => (
+                                        <>
+                                            <div key={index} className={'flex'}>
+                                                <div className={'w-[200px]'}>
+                                                    <select
+                                                        value={selectedStaff?.vehicle_list[index]}
+                                                        onChange={(e) => {
+                                                            setSelectedVehicleId(index, e)
+                                                        }}
+                                                        key={index}
+                                                        className="mt-2 mb-2 form-control p-2 rounded-md w-full text-gray-500"
+                                                    >
+                                                        {vehicleIds.map((e) => (
+                                                            <option key={e} value={e}>{e}</option>
+                                                        ))}
+                                                    </select>
+                                                </div>
+                                                <div>
+                                                    <button
+                                                        key={index}
+                                                        onClick={() => {
+                                                            removeVehicleIdSelector(index)
+                                                        }}
+                                                        className={'mt-2 ml-4 h-[40px] w-[70px] bg-green-500 text-white rounded-md hover:bg-red-500'}>Remove
+                                                    </button>
+                                                </div>
+
+
+                                            </div>
+                                        </>
+                                    ))}
+                                </div>
+                                <div>
+
+                                </div>
+
+                            </div>
 
                             {/* Add additional input fields in similar manner */}
                             {/*<div>*/}
