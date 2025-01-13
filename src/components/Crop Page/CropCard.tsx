788cae419d4e992d58f95a8e0b0d66cfd919a47f
Amodhx <amxdhnanditha@gmail.com>
2025-01-13 14:43:06 +0530

    setted values to crop card

--- src/components/Crop Page/CropCard.tsx
+++ src/components/Crop Page/CropCard.tsx
@@ -1,4 +1,3 @@
-import cropImg from '../../assets/images.jpg'
 import CardButtons from "../CardButtons.tsx";
 import {useState} from "react";
 import CropModel from "../../model/CropModel.ts";
@@ -42,23 +41,23 @@ function CropCard({crop}: {crop: CropModel}) {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
                 <div className="bg-gray-800 text-white border border-gray-400 w-[310px] rounded-md overflow-hidden">
                     <img
-                        src={cropImg}
+                        src={crop.crop_image || undefined}
                         alt="Crop Image"
                         className="w-full h-[175px] object-cover"
                     />
                     <div className="p-4">
-                        <h5 className="text-lg font-bold mb-2">Dummy Crop Name</h5>
+                        <h5 className="text-lg font-bold mb-2">{crop.crop_common_name}</h5>
                         <p className="text-sm mb-2">
-                            <strong>Scientific Name:</strong> Dummy Scientific Name
+                            <strong>Scientific Name:</strong> {crop.crop_scientific_name}
                         </p>
                         <p className="text-sm mb-2">
-                            <strong>Category:</strong> Dummy Category
+                            <strong>Category:</strong> {crop.category}
                         </p>
                         <p className="text-sm mb-2">
-                            <strong>Crop Season:</strong> Dummy Season
+                            <strong>Crop Season:</strong> {crop.season}
                         </p>
                         <p className="text-sm mb-2">
-                            <strong>Field ID:</strong> DummyField123
+                            <strong>Field ID:</strong> {crop.field_list}
                         </p>
                         <CardButtons onUpdateCropClick={onUpdateClick} onDeleteCropClick={onDeleteClick}/>
                     </div>
