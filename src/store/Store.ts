bbe711011f08e14b5f8bc57ed56439426cb94ce2
Amodhx <amxdhnanditha@gmail.com>
2025-01-13 10:49:01 +0530

    added crops to store

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 import VehicleSlice from "../slices/VehicleSlice.ts";
 import EquipmentSlice from "../slices/EquipmentSlice.ts";
 import StaffSlice from "../slices/StaffSlice.ts";
+import CropSlice from "../slices/CropSlice.ts";
 
 export const store = configureStore({
     reducer:{
         vehicles : VehicleSlice,
         equipments : EquipmentSlice,
-        staffs : StaffSlice
+        staffs : StaffSlice,
+        crops : CropSlice
     }
 })
