a2a6ac4581709f2ac64584b539e992776876ea87
Amodhx <amxdhnanditha@gmail.com>
2025-01-13 14:35:24 +0530

    delete crop done

--- src/components/Crop Page/CropCard.tsx
+++ src/components/Crop Page/CropCard.tsx
@@ -3,13 +3,36 @@ import CardButtons from "../CardButtons.tsx";
 import {useState} from "react";
 import CropModel from "../../model/CropModel.ts";
 import CropModal from "./CropModal.tsx";
+import {useDispatch} from "react-redux";
+import {deleteCrop} from "../../slices/CropSlice.ts";
+import Swal from "sweetalert2";
 function CropCard({crop}: {crop: CropModel}) {
     const [selectedCrop, setSelectedCrop] = useState<CropModel | null>(null)
+    const dispatch = useDispatch();
     function onUpdateClick(){
         setSelectedCrop(crop);
     }
     function onDeleteClick(){
-
+        if (crop){
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    dispatch(deleteCrop(crop))
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+            });
+        }
     }
     function closeModal(){
         setSelectedCrop(null)
