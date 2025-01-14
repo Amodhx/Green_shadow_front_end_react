0c186d0e5196d2589c4edc07746e3efc6645fd40
Amodhx <amxdhnanditha@gmail.com>
2025-01-14 11:27:55 +0530

    done log save,update,delete

--- src/pages/Logs.tsx
+++ src/pages/Logs.tsx
@@ -1,8 +1,34 @@
+import {useSelector} from "react-redux";
+import {useState} from "react";
+import AddNewButton from "../components/AddNewButton.tsx";
+import LogCard from "../components/Logs Page/LogCard.tsx";
+import LogModel from "../model/LogModel.ts";
+import LogModal from "../components/Logs Page/LogModal.tsx";
+
 function Logs(){
+    const logs = useSelector((state : any) => state.logs)
+    const [isModalShow, setIsModalShow] = useState(false)
+    const openModal = () => setIsModalShow(true);
+    const closeModal = () => setIsModalShow(false);
     return(
         <>
-            <h1>Logs Page</h1>
+            <div className="flex items-center justify-between w-full h-[6rem] bg-gray-50 p-4 mt-4">
+                <div>
+                    <h1 className="block text-xl font-semibold text-gray-700">Logs Details</h1>
+                </div>
+                <AddNewButton openModal={openModal} text={'Add New Log'}/>
+            </div>
+            <div className="grid grid-cols-4 gap-5 p-5">
+                {logs.map((log : LogModel) =>(
+                    <LogCard log={log}/>
+                ))}
+            </div>
+
+            {isModalShow && (
+                <LogModal closeModal={closeModal} selectedLog={null}/>
+            )}
         </>
     )
 }
+
 export default Logs;
