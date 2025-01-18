c0f8a6a17e4a3d8eefefd72318e2a515d32650a9
Amodhx <amxdhnanditha@gmail.com>
2025-01-18 10:21:30 +0530

    field card created

---
+++ src/components/Field Page/FieldCard.tsx
@@ -0,0 +1,81 @@
+import FieldModel from "../../model/FieldModel.ts";
+import {useState} from "react";
+import Swal from "sweetalert2";
+import CardButtons from "../CardButtons.tsx";
+import FieldModal from "./FieldModal.tsx";
+import {useDispatch} from "react-redux";
+import {deleteField} from "../../slices/FieldSlice.ts";
+
+function FieldCard({field} : {field: FieldModel}){
+
+    const [selectedField, setSelectedField] = useState<FieldModel | null>(null)
+    const dispatch = useDispatch();
+    function onUpdateClick(){
+        setSelectedField(field);
+    }
+    function onDeleteClick(){
+        if (field){
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    dispatch(deleteField(field))
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+            });
+        }
+    }
+    function closeModal(){
+        setSelectedField(null)
+    }
+
+    return(
+        <>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+                <div className="bg-gray-800 text-white border border-gray-400 w-[310px] rounded-md overflow-hidden">
+                    <img
+                        src={field.field_image || undefined}
+                        alt="field Image"
+                        className="w-full h-[175px] object-cover"
+                    />
+                    <div className="p-4">
+                        <h5 className="text-lg font-bold mb-2">{field.field_name}</h5>
+                        <p className="text-sm mb-2">
+                            <strong>Field Location:</strong> {field.filed_location}
+                        </p>
+                        <p className="text-sm mb-2">
+                            <strong>Extent Size:</strong> {field.extent_size}
+                        </p>
+                        <p className="text-sm mb-2">
+                            <strong>Staffs :</strong> {field.staff_list}
+                        </p>
+                        <p className="text-sm mb-2">
+                            <strong>Crops :</strong> {field.crops_list}
+                        </p>
+                        <p className="text-sm mb-2">
+                            <strong>Equipments :</strong> {field.equipment_list}
+                        </p>
+                        <CardButtons onUpdateCropClick={onUpdateClick} onDeleteCropClick={onDeleteClick}/>
+                    </div>
+                </div>
+            </div>
+            {selectedField && (
+                <FieldModal closeModal={closeModal} selectedField={field}/>
+            )}
+        </>
+
+
+    )
+}
+
+export default FieldCard;
--- src/pages/Fields.tsx
+++ src/pages/Fields.tsx
@@ -1,8 +1,33 @@
+import FieldModel from "../model/FieldModel.ts";
+import {useSelector} from "react-redux";
+import {useState} from "react";
+import AddNewButton from "../components/AddNewButton.tsx";
+import FieldCard from "../components/Field Page/FieldCard.tsx";
+import FieldModal from "../components/Field Page/FieldModal.tsx";
+
 function Fields(){
+    const fields : FieldModel[] = useSelector((state : any)=>state.fields);
+    const [isModalShow, setIsModalShow] = useState(false)
+    const openModal = () => setIsModalShow(true);
+    const closeModal = () => setIsModalShow(false);
     return(
         <>
-            <h1>Fields Page</h1>
+            <div className="flex items-center justify-between w-full h-[6rem] bg-gray-50 p-4 mt-4">
+                <div>
+                    <h1 className="block text-xl font-semibold text-gray-700">Fields Details</h1>
+                </div>
+                <AddNewButton openModal={openModal} text={'Add New Field'}/>
+            </div>
+            <div className="grid grid-cols-4 gap-5 p-5">
+                {fields.map((field) => (
+                    <FieldCard field={field}/>
+                ))}
+            </div>
+            {isModalShow && (
+                <FieldModal closeModal={closeModal} selectedField={null}/>
+            )}
         </>
     )
 }
+
 export default Fields
